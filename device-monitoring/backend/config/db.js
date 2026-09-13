a34fadfb9e4d0232306156741cab236a8134b1db
Muhammad Akmalluddin <makmalluddin123@gmail.com>
2026-09-14 03:37:56 +0700

    feat: create connection to postgresql

--- device-monitoring/backend/config/db.js
+++ device-monitoring/backend/config/db.js
@@ -4,16 +4,24 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 // Pool postgresql 
-const { Pool } = pkg;
+const { Pool } = pg;
 
 // Connect db 
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  password: process.env.DB_PSWD,
+  database: process.env.DB_NAME,
 });
 
+pool.connect((error, client, release) => {
+  if (error) {
+    console.log('Gagal connect', error)
+  } else {
+    console.log('Berhasil terhubung')
+  };
+  if (client) release();
+});
 
-
+export default pool;
--- device-monitoring/backend/server.js
+++ device-monitoring/backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import './config/db.js';
 
 // Buat aplikasi 
 const port = process.env.PORT;
